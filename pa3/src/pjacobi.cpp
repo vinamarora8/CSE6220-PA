6ee70cdbe56c552a1fd35cf40335cc48e02c7070
Vinam Arora <vinam@posteo.de>
2023-04-12 13:04:25 -0400

    mat_vec_mult bugfix + Adds DBGMSG

--- pa3/src/pjacobi.cpp
+++ pa3/src/pjacobi.cpp
@@ -5,10 +5,17 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <cassert>
 #include <mpi.h>
 
 #define EPS 1e-9
 #define MAX_ITER 1000000
+
+#ifdef NDEBUG
+#   define DBGMSG(en, msg)
+#else
+#   define DBGMSG(en, msg) {if (en) (std::cout << msg << std::endl);}
+#endif
 
 // Types
 typedef std::vector<std::vector<double>> Mat;
@@ -40,6 +47,8 @@ std::string g2s(const GridInfo &g); // Converts grid coords to string
 
 int main(int argc, char *argv[])
 {
+    bool debug = true;
+
     char *in_mat_fname = argv[1];
     char *in_vec_fname = argv[2];
     char *op_fname = argv[3];
@@ -54,12 +63,13 @@ int main(int argc, char *argv[])
     grid_info.rank = rank;
     grid_info.world_size = world_size;
     grid_info.grid_size = sqrt(world_size);
-    int periods[2] = {1, 1};
+    assert(grid_info.grid_size * grid_info.grid_size == world_size);
+    int periods[2] = {0, 0};
     int dims[2] = {grid_info.grid_size, grid_info.grid_size};
     MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 1, &grid_info.grid_comm);
     MPI_Cart_coords(grid_info.grid_comm, rank, 2, grid_info.grid_coords);
 
-    std::cout << "Rank " << rank << " coords: " << g2s(grid_info) << std::endl;
+    DBGMSG(debug, "Rank " << rank << " coords: " << g2s(grid_info));
     MPI_Barrier(grid_info.grid_comm);
 
     // Read input matrix and vector
@@ -151,6 +161,8 @@ double compute_error(const Mat &A, const Vec &x, const Vec &b, const GridInfo &g
  */
 void mat_vec_mult(Vec &y, const Mat &A, const Vec &x, const GridInfo &g, bool ign_diag)
 {
+    bool debug = false;
+
     // Create x_t on all processes.
     Vec x_t(A[0].size());
     // Two steps:
@@ -161,7 +173,7 @@ void mat_vec_mult(Vec &y, const Mat &A, const Vec &x, const GridInfo &g, bool ig
             // Copy
             // Separate so that Send and Recv don't overlap for the same process
             x_t = x;
-            std::cout << g2s(g) << " copied x" << std::endl;
+            DBGMSG(debug, g2s(g) << " copied x");
         }
         else if (g.grid_coords[1] == 0)
         {
@@ -169,8 +181,8 @@ void mat_vec_mult(Vec &y, const Mat &A, const Vec &x, const GridInfo &g, bool ig
             int recv_rank;
             int recv_coord[2] = {g.grid_coords[0], g.grid_coords[0]};
             MPI_Cart_rank(g.grid_comm, recv_coord, &recv_rank);
-            std::cout << g2s(g) << " send to " << recv_rank << std::endl;
             MPI_Send(&x[0], x.size(), MPI_DOUBLE, recv_rank, 0, g.grid_comm);
+            DBGMSG(debug, g2s(g) << " send to " << recv_rank);
         }
         else if (g.grid_coords[0] == g.grid_coords[1])
         {
@@ -178,22 +190,23 @@ void mat_vec_mult(Vec &y, const Mat &A, const Vec &x, const GridInfo &g, bool ig
             int send_rank;
             int send_coord[2] = {g.grid_coords[0], 0};
             MPI_Cart_rank(g.grid_comm, send_coord, &send_rank);
-            std::cout << g2s(g) << " receive from " << send_rank << std::endl;
             MPI_Recv(&x_t[0], x_t.size(), MPI_DOUBLE, send_rank, 0, g.grid_comm, MPI_STATUS_IGNORE);
+            DBGMSG(debug, g2s(g) << " receive from " << send_rank);
         }
     }
     // 2. Broadcast from diagonal
     {
         // Split grid communicator into column communicator
         MPI_Comm col_comm;
-        int remain_dims[2] = {0, 1}; // keep column dimension
+        int remain_dims[2] = {1, 0}; // drop column dimension
         MPI_Cart_sub(g.grid_comm, remain_dims, &col_comm);
         // Find rank of diagonal
         int diag_rank;
-        int diag_coord = g.grid_coords[0];
-        MPI_Cart_rank(col_comm, &diag_coord, &diag_rank);
+        int diag_coord[1] = {g.grid_coords[0]};
+        MPI_Cart_rank(col_comm, diag_coord, &diag_rank);
         // Broadcast 
         MPI_Bcast(&x_t[0], x_t.size(), MPI_DOUBLE, diag_rank, col_comm);
+        DBGMSG(debug, g2s(g) << " got x_t from " << diag_rank);
     }
 
     // Local dot product
@@ -213,14 +226,15 @@ void mat_vec_mult(Vec &y, const Mat &A, const Vec &x, const GridInfo &g, bool ig
     // Reduce local_y to y
     // Split grid communicator into row communicator
     MPI_Comm row_comm;
-    int remain_dims[2] = {1, 0}; // keep row dimension
+    int remain_dims[2] = {0, 1}; // drop row dimension 
     MPI_Cart_sub(g.grid_comm, remain_dims, &row_comm);
     // Find rank of column 0
     int root_rank;
-    int root_coord = 0;
-    MPI_Cart_rank(row_comm, &root_coord, &root_rank);
+    int root_coord[1] = {0};
+    MPI_Cart_rank(row_comm, root_coord, &root_rank);
     // Reduce
-    MPI_Reduce(&local_y[0], &y[0], y.size(), MPI_DOUBLE, MPI_SUM, root_rank, row_comm);
+    MPI_Reduce(&local_y[0], &y[0], local_y.size(), MPI_DOUBLE, MPI_SUM, root_rank, row_comm);
+    DBGMSG(debug, g2s(g) << " reduced to " << root_rank);
 }
 
 
