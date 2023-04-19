133692f30c16e380907346506ed365fa1802d7ab
Vinam Arora <vinam@posteo.de>
2023-04-18 22:21:38 -0400

    Adds test_matvecmult.py

---
+++ pa3/tests/test_matvecmult.py
@@ -0,0 +1,76 @@
+import numpy as np
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('nprocs', type=int, help="Number of CPUs")
+parser.add_argument('--n', type=int, default=None, help="Problem size (n)")
+parser.add_argument('--debug', type=bool, default=False, help="Debug output")
+args = parser.parse_args()
+
+nprocs = args.nprocs
+n = args.n
+assert(int(np.sqrt(nprocs))**2 == nprocs)
+
+if n is None:
+    n = np.random.randint(4, 100)
+A = np.random.random((n, n)) * 10
+x = np.random.random(n) * 100
+
+y = np.dot(A, x)
+
+def write_matrix(A, fname):
+    n = A.shape[0]
+    with open(fname, 'w') as f:
+        f.write(f'{n}\n')
+        for i in range(n):
+            for j in range(n):
+                f.write(f'{A[i, j]} ')
+            f.write('\n')
+
+def write_vector(x, fname):
+    with open(fname, 'w') as f:
+        for i in range(len(x)):
+            f.write(f'{x[i]} ')
+        f.write('\n')
+
+def read_vector(fname, n):
+    with open(fname, 'r') as f:
+        text = f.readlines()[0].strip()
+    text_double = [float(x) for x in text.split(' ')]
+    ans = np.array(text_double)
+    return ans
+
+
+inp_mat_fname = 'inp_mat.txt'
+inp_vec_fname = 'inp_vec.txt'
+out_fname = 'out_vec.txt'
+
+write_matrix(A, inp_mat_fname)
+write_vector(x, inp_vec_fname)
+
+cmd = f'mpirun -np {nprocs} ./pjacobi {inp_mat_fname} {inp_vec_fname} {out_fname}'
+print(cmd)
+os.system(cmd)
+
+ans = read_vector('out_vec.txt', n)
+err = y - ans
+err_norm = np.linalg.norm(err)
+
+if args.debug:
+    print('Expected')
+    print(y)
+
+    print('Actual')
+    print(ans)
+
+    print(f'Error:')
+    print(err)
+
+print(f'Error norm: {err_norm}')
+if (err_norm < 1e-9):
+    print('PASS')
+    exit(0)
+else:
+    print('FAIL')
+    exit(1)
