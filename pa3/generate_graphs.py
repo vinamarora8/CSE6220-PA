a750896628a59bf1ef6601061873c85a1291463a
gatech <23293632+pratiksha-pai@users.noreply.github.com>
2023-04-22 23:12:55 -0400

    Update generate_graphs.py

--- pa3/generate_graphs.py
+++ pa3/generate_graphs.py
@@ -2,44 +2,55 @@
 import subprocess
 import numpy as np
 import matplotlib.pyplot as plt
-import io 
-import sys
-import os
 import re
-
 from test import one
 
 
-# procs = [1, 4, 9, 16, 25, 36, 49, 64]
-procs = [1, 4,]# 9, 16, 25, 36, 49, 64]
-# nums = [10, 100, 1000, 10000, 100000, 1000000]
-nums = [10, 100, ] #1000, 10000, 100000, 1000000]
+procs = [4, 9, 16, 25, 36, 49]
+nums = [10, 50, 100, 300, 500, 1000] 
 
-'''
-get runtime vs p
-for n = 10, 100, 1000, 10000, 100000, 1000000
-for p = 1, 4, 9, 16, 25, 36, 49, 64
-plot runtime vs p for each n
-'''
+
+def util(n, p):
+    # run test.py with n, p  
+    cmd = f'python test.py {p} --n {n}'
+    print(cmd)
+    
+    # test once so that the output is not messed up
+    try:
+        output = subprocess.check_output(cmd, shell=True)
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+    
+    count = 0
+    sum = 0
+    temp_runtime = 0
+
+    for i in range(3):
+        output = subprocess.check_output(cmd, shell=True)
+        output_lines = output.decode('utf-8').split('\n')
+        pattern = r"Runtime: (\d+\.\d+)"
+        match = re.search(pattern, output_lines[0])
+        if match != None:
+            count += 1
+            sum += float(match.group(1))
+    if count != 0:
+        temp_runtime = sum/count
+    else:
+        print("For n = ", n, " and p = ", p, " count = ", count)
+    return temp_runtime
+
+
 def runtime_vs_p():
 
     avg_runtimes = []
     for n in nums:
         temp_runtimes = []
         for p in procs:
-            sum = 0
-            # find the mean of 5 runs
-            cmd = f'python test.py {p} --n {n}'
-            print(cmd)
-            for i in range(5):
-                # run test.py with n, p
-                output = subprocess.check_output(cmd, shell=True)
-                output_lines = output.decode('utf-8').split('\n')
-                pattern = r"Runtime: (\d+\.\d+)"
-                match = re.search(pattern, output_lines[0])
-                sum += float(match.group(1))
-            temp_runtimes.append(sum/5)
-        avg_runtimes.append(temp_runtimes)
+            temp_runtimes.append(util(n, p))
+        print(temp_runtimes)
+    
+    avg_runtimes.append(temp_runtimes)
+    print(avg_runtimes)
 
     # plot runtime vs p for each n 
     # juxtapose all plots
@@ -56,34 +67,16 @@ def runtime_vs_p():
     plt.clf()
 
 
-'''
-get runtime vs n
-for n = 10, 100, 1000, 10000, 100000, 1000000
-for p = 1, 4, 9, 16, 25, 36, 49, 64
-plot runtime vs n for each p
-'''
 def runtime_vs_n():
-
-
-    
     avg_runtimes = []
     for p in procs:
         temp_runtimes = []
         for n in nums:
-            sum = 0
-            cmd = f'python test.py {p} --n {n}'
-            print(cmd)
-            # find the mean of 5 runs
-            for i in range(5):
-                # run test.py with n, p
-                
-                output = subprocess.check_output(cmd, shell=True)
-                output_lines = output.decode('utf-8').split('\n')
-                pattern = r"Runtime: (\d+\.\d+)"
-                match = re.search(pattern, output_lines[0])
-                sum += float(match.group(1))
-            temp_runtimes.append(sum/5)
-        avg_runtimes.append(temp_runtimes)
+            temp_runtimes.append(util(n, p))
+        print(temp_runtimes)
+    
+    avg_runtimes.append(temp_runtimes)
+    print(avg_runtimes)
 
     # plot runtime vs n for each p
     # juxtapose all plots
@@ -92,7 +85,7 @@ def runtime_vs_n():
         plt.scatter(nums, avg_runtimes[i])
     plt.grid()
     plt.yscale('log')
-    plt.xscale('log', base=10)
+    plt.xticks(nums, nums)
     plt.xlabel('Number of elements (n)')
     plt.ylabel('Run-time (ms)')
     plt.legend()
